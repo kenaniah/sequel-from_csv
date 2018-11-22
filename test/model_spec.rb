24a7db06bbaffb87d9052f7a4251344de1e40e21
Kenaniah Cerny <kenaniah@gmail.com>
2018-11-22 12:20:19 -0800

    temp table

--- test/model_spec.rb
+++ test/model_spec.rb
@@ -11,7 +11,7 @@ describe "Plugin" do
       DB.execute <<-SQL
 
         -- A simple base table
-        CREATE TABLE simple_tests(
+        CREATE TEMP TABLE simple_tests(
           id SERIAL PRIMARY KEY,
           name TEXT NOT NULL,
           is_active BOOLEAN,
