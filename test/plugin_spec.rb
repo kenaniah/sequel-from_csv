436f512cc6e3891046f9f5a29680a101e74afe52
Kenaniah Cerny <kenaniah@gmail.com>
2018-11-21 17:23:01 -0800

    [ci skip] schema checkpoint

--- test/plugin_spec.rb
+++ test/plugin_spec.rb
@@ -2,6 +2,48 @@ require "test_helper"
 
 describe "Plugin" do
 
+  def with_connection
+    return skip("No DATABASE_URL env variable defined") unless ENV['DATABASE_URL']
+    @db ||= Sequel.connect ENV['DATABASE_URL']
+    @random = rand 10000...10000
+    @db.transaction rollback: :always do
+      @db.run <<-SQL
+
+        CREATE SCHEMA test_#{@random};
+
+        -- A simple base table
+        CREATE TABLE simple_#{@random}(
+          id SERIAL PRIMARY KEY,
+          name TEXT NOT NULL,
+          is_active BOOLEAN,
+          created_at NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+
+        -- A namespaced table
+        CREATE TABLE test_#{@random}.namespaced(
+          key1 TEXT PRIMARY KEY,
+          field1 INTERVAL,
+          field2 TEXT
+        );
+
+        -- A table with a compound key
+        CREATE TABLE test_#{@random}.compound(
+          key1 INTEGER NOT NULL,
+          key2 INTEGER NOT NULL,
+          field1 TEXT
+        );
+
+        -- A table with no keys
+        CREATE TABLE test_#{@random}.keyless(
+          field1 TEXT,
+          field2 TEXT
+        );
+
+      SQL
+      yield
+    end
+  end
+
   it "should expose a #seed_from_csv method on model instances" do
     db = Sequel.mock
     class MockModel < Sequel::Model(db); end
@@ -11,7 +53,9 @@ describe "Plugin" do
   end
 
   it "should return self when #seed_from_csv is called" do
-    pass
+    with_connection do
+      assert true
+    end
   end
 
   it "should raise not implemented for compound primary keys" do
